56bc3dc69164aa0591355221ed2967603c9c320a
insomniac807 <philipbutler25@gmail.com>
2020-08-13 05:13:57 +0100

    added config file, got bot to respond to non command messages

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 app = Flask(__name__)
-from telegram.ext import Updater, CommandHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from config import Config
 import logging
 
-TOKEN = '1235895745:AAGGubtpiZnSzEUwbDGFRZvnXOiD237WI5I'
+TOKEN = Config.TOKEN
 
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
@@ -15,6 +16,12 @@ def start(update, context):
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
+
+def echo(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text=update.message.text)
+    
+echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+dispatcher.add_handler(echo_handler)
 
 if __name__ == '__main__':
     updater.start_polling()
---
+++ config.example.py
@@ -0,0 +1,4 @@
+# change name to config.py and change parameters
+
+class Config:
+    TOKEN = 'YOUR_TOKEN_HERE'
