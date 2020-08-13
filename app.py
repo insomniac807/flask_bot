73b0089c4cc5b560fb0734ee1d072c1739eedf67
insomniac807 <philipbutler25@gmail.com>
2020-08-13 03:47:06 +0100

    added python-telegram-bot

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
 app = Flask(__name__)
+import telegram
+bot = telegram.Bot(token='1235895745:AAGGubtpiZnSzEUwbDGFRZvnXOiD237WI5I')
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
+    mybot = str(bot.get_me())
+    return "Hello World!"+mybot
 
 if __name__ == '__main__':
     app.run()
