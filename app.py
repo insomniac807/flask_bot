8b2189f55ea946b845e1258809ab09b2e0a2768b
insomniac807 <philipbutler25@gmail.com>
2020-08-13 08:11:35 +0100

    started working on quotes module

--- app.py
+++ app.py
@@ -1,22 +1,30 @@
 from flask import Flask
-from karma import Karma
-app = Flask(__name__)
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from config import Config
+from karma import Karma
 import redis
+
+app = Flask(__name__)
 
 TOKEN = Config.TOKEN
 
 r = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+
 updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
+################                       WORKING ON ALL THIS QUOTY STUFF HEREREEREEE ITS EAAASSSYYYYY              #################################
+def quote(update, context):
+    params = ["add", "search", "delete", "rmlast"]
+    args = context.args
+    if args[0] in params:
+        print(args)
+    else:
+        context.bot.send_message(chat_id=update.effective_chat.id, text="Second parameter looks dodgy there pal <.<")
 
-def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
+quote_handler = CommandHandler('q', quote)
+dispatcher.add_handler(quote_handler)
+###################################################################################################################################################
 
 def parse_incoming_message(update, context):
     if "++" in update.message.text or "—" in update.message.text or update.message.text.split(" ")[0] == "karma":
