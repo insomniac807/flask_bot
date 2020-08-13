0ccfc971a9c744cc1f3537469b69e5e87627e20a
insomniac807 <philipbutler25@gmail.com>
2020-08-13 04:19:12 +0100

    basic bot functionality

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
 from flask import Flask
 app = Flask(__name__)
-import telegram
-bot = telegram.Bot(token='1235895745:AAGGubtpiZnSzEUwbDGFRZvnXOiD237WI5I')
+from telegram.ext import Updater, CommandHandler
+import logging
 
+TOKEN = '1235895745:AAGGubtpiZnSzEUwbDGFRZvnXOiD237WI5I'
+
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s-%(name)s-%(messages)s', level=logging.INFO)
+
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+start_handler = CommandHandler('start', start)
+dispatcher.add_handler(start_handler)
 
 @app.route('/')
 def hello():
-    mybot = str(bot.get_me())
-    return "Hello World!"+mybot
+    updater.start_polling()
+    return "Hello World!"
 
 if __name__ == '__main__':
+    updater.start_polling()
     app.run()
